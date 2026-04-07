432ab00a1f6ad0f804d20e4271d6bac204108394
zedtopvibes <zedtopvibes@gmail.com>
2026-04-07 14:38:19 +0200

    Create src/commands/start.js

---
+++ src/commands/start.js
@@ -0,0 +1,15 @@
+export async function handleStart(chatId, firstName, env) {
+  const BOT_TOKEN = env.BOT_TOKEN;
+  const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+  
+  const response = await fetch(`${TELEGRAM_API}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: `Hello ${firstName}! Bot is working.`
+    })
+  });
+  
+  return response.json();
+}
